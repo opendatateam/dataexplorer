aaf17420cccfce8254cd4d1a2d699c2f4df93ea2
Axel Haustant <noirbizarre@gmail.com>
2018-09-04 17:18:40 +0200

    Extract config from meta

--- src/utilis/config.js
+++ src/utilis/config.js
@@ -1,1 +1,16 @@
-export const serverApi = 'https://csvapi.maboiteprivee.fr:8080'
+/**
+ * Simple helper to fetch attribute on given css selector
+ */
+function _attr(selector, name) {
+    const el = document.querySelector(selector);
+    return el ? el.getAttribute(name) : undefined;
+}
+
+/**
+ * Simple helper to <meta/> tag content given its name
+ */
+function _meta(name) {
+    return _attr(`meta[name=${name}]`, 'content');
+}
+
+export const serverApi = _meta('csvapi-url');
